8c0fefa184bea879ad622c0ab5cf7980e0458675
KJM <kjm99d@naver.com>
2022-10-20 15:49:58 +0900

    프로젝트명 수정 - 테스트 프로젝트 이름 수정

---
+++ LauncherEXE/CoffeeLauncherTest/src/main.cpp
@@ -0,0 +1,41 @@
+#include <Windows.h>
+#include <WinHttpComm/include/Request.h>
+#pragma comment(lib, "WinHttpComm.lib")
+
+
+#include <windows.h>
+#include <iostream>
+
+
+
+void MyCreateProcess()
+{
+	STARTUPINFO si = { sizeof(si) };
+
+	PROCESS_INFORMATION pi;
+
+	TCHAR szCommandLine[] = TEXT("NOTEPAD");
+	CreateProcess(NULL, szCommandLine, NULL, NULL,
+		FALSE, 0, NULL, NULL, &si, &pi);
+}
+
+int main()
+{
+//	POST();
+	
+
+	CRequest& request = CRequest::GetInst();
+	request.SetMethod(kGET);
+	request.Request();
+	
+	MyCreateProcess();
+	OutputDebugString(L"aa");
+	std::cout << "sadkjnsadksadsadasdkjn" << std::endl;
+
+	while (true)
+	{
+
+	}
+
+	return 0;
+}
