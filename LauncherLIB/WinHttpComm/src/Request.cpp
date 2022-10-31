8c85946560aaf60a5a3c00f68a845ef44444aa71
KJM <kjm99d@naver.com>
2022-10-31 21:35:09 +0900

    WinHttpComm 코드 리팩토링 - 레거시 코드 제거 - application/x-www-form-urlencoded 지원

--- LauncherLIB/WinHttpComm/src/Request.cpp
+++ LauncherLIB/WinHttpComm/src/Request.cpp
@@ -5,53 +5,11 @@
 #pragma comment (lib, "winhttp.lib")
 
 // ======================================================================= //
-#if 0
-CRequest::CRequest(const wchar_t* url)
-{
-	OutputDebugString(L"CRequest Init()");
-
-	hSession = NULL;
-
-
-	// 멤버 변수 URL을 설정하고
-	SetURL(url);
-	// 세션을 할당받고
-	//Open();
-	// 컴포넌트 파싱
-	//SetComponent();
-
-	const wchar_t* wcsUrl = m_url;
-	if (!WinHttpCrackUrl(wcsUrl, lstrlenW(wcsUrl), 0, &m_urlComponents)) {
-		WinHttpCloseHandle(hSession);
-		//return -2;
-	}
-
-	// 커넥션 열고
-	const INTERNET_PORT nPort = m_urlComponents.nPort; /* WORD */
-	m_hConnect = WinHttpConnect(hSession, m_szHostName, nPort, 0);
-	if (m_hConnect == NULL) {
-		WinHttpCloseHandle(hSession);
-		//return -3;
-	}
-	
-}
-
-CRequest::CRequest(const RequestMethod method, wchar_t* url)
-{
-	
-}
-
-#endif
 
 CRequest::~CRequest()
 {
 	OutputDebugString(L"CRequest destroy()");
 	Close();
-}
-
-RequestError CRequest::GetLastError()
-{
-	return RequestError();
 }
 
 void CRequest::Open(RequestMethod method, const wchar_t* url)
@@ -82,8 +40,23 @@ void CRequest::Open(RequestMethod method, const wchar_t* url)
 
 void CRequest::Send(const wchar_t* form)
 {
+	int nLenForm = 0;
+	LPVOID lpForm = NULL;
+	if (form)
+	{
+		nLenForm = lstrlenW(form);
 
-	if (!WinHttpSendRequest(m_hRequest, WINHTTP_NO_ADDITIONAL_HEADERS, 0, WINHTTP_NO_REQUEST_DATA, 0, 0, 0)) {
+		char strTxt[2048] = {};
+		WideCharToMultiByte(CP_ACP, 0, form, -1, strTxt, 2048, NULL, NULL);
+
+		// Content-Type 헤더를 쓴다.
+		SetHeader(L"Content-Type", L"application/x-www-form-urlencoded");
+		lpForm = strTxt;
+	}
+
+
+
+	if (!WinHttpSendRequest(m_hRequest, WINHTTP_NO_ADDITIONAL_HEADERS, 0, lpForm, nLenForm, nLenForm, 0)) {
 		DWORD dwError = GetLastError();
 		WinHttpCloseHandle(m_hRequest);
 		WinHttpCloseHandle(m_hConnect);
@@ -102,70 +75,6 @@ void CRequest::Send(const wchar_t* form)
 		&dwSize, WINHTTP_NO_HEADER_INDEX);
 }
 
-int CRequest::Request()
-{
-	
-	//m_responseBody.clear();
-
-	DWORD          dwSize;
-
-
-	const wchar_t* wcsMethod = StrRequestMethodW();
-	m_hRequest = WinHttpOpenRequest(
-		m_hConnect, wcsMethod, m_szUrlPath, 
-		NULL, WINHTTP_NO_REFERER, WINHTTP_DEFAULT_ACCEPT_TYPES, 0);
-	if (m_hRequest == NULL) {
-		WinHttpCloseHandle(m_hConnect);
-		WinHttpCloseHandle(hSession);
-		return -4;
-	}
-
-	/*
-	* Header Iterator 
-	* 헤더를 순회하면서, 헤더값을 추가해 서버에 요청한다.
-	*/
-	for (auto& header : m_headers)
-	{
-		const wchar_t* key = header.first;
-		const wchar_t* value = header.second;
-
-		wchar_t szHeader[2048] = { 0, };
-		swprintf_s(szHeader, 2048, L"%s: %s", key, value);
-
-		WinHttpAddRequestHeaders(m_hRequest, szHeader, -1L, WINHTTP_ADDREQ_FLAG_ADD);
-
-	}
-	
-
-	if (!WinHttpSendRequest(m_hRequest, WINHTTP_NO_ADDITIONAL_HEADERS, 0, WINHTTP_NO_REQUEST_DATA, 0, 0, 0)) {
-		DWORD dwError = GetLastError();
-		WinHttpCloseHandle(m_hRequest);
-		WinHttpCloseHandle(m_hConnect);
-		WinHttpCloseHandle(hSession);
-		return -5;
-	}
-
-	WinHttpReceiveResponse(m_hRequest, NULL);
-
-	dwSize = sizeof(DWORD);
-	WinHttpQueryHeaders(m_hRequest, 
-		WINHTTP_QUERY_STATUS_CODE | WINHTTP_QUERY_FLAG_NUMBER, 
-		WINHTTP_HEADER_NAME_BY_INDEX, 
-		&m_dwStatusCode, 
-		&dwSize, WINHTTP_NO_HEADER_INDEX);
-
-	
-	
-	return 0;
-
-
-}
-
-void CRequest::SetMethod(const RequestMethod method)
-{
-	m_method = method;
-}
-
 void CRequest::SetHeader(const wchar_t* key, const wchar_t* value)
 {
 
@@ -175,25 +84,6 @@ void CRequest::SetHeader(const wchar_t* key, const wchar_t* value)
 	BOOL status = WinHttpAddRequestHeaders(m_hRequest, szHeader, -1L, WINHTTP_ADDREQ_FLAG_ADD);
 }
 
-void CRequest::SetHeader(request_header headers)
-{
-	m_headers = headers;
-}
-
-void CRequest::SetPayload(const wchar_t* payload)
-{
-}
-
-void CRequest::SetURL(const wchar_t* url)
-{
-	// 메모리 초기화
-	RtlZeroMemory(m_url, sizeof(m_url) / sizeof(WCHAR));
-	// 문자열 복사
-	swprintf_s(m_url, 4096, L"%s", url);
-
-	//OutputDebugString(m_url);
-}
-
 
 const wchar_t* CRequest::StrRequestMethodW(const RequestMethod method)
 {
@@ -201,7 +91,7 @@ const wchar_t* CRequest::StrRequestMethodW(const RequestMethod method)
 	const WCHAR* wcsPost	= L"POST";
 	const WCHAR* wcsDelete	= L"DELETE";
 	const WCHAR* wcsPut		= L"PUT";
-	const WCHAR* wcsPatch	= L"wcsPatch";
+	const WCHAR* wcsPatch	= L"PATCH";
 
 	const WCHAR* strMethod;
 
