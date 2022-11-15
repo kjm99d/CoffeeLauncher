a614e07eb19b2f2b7424b384cb20c3c833e904ac
KJM <kjm99d@naver.com>
2022-11-15 14:56:20 +0900

    라이센스 관련 기능 추가

---
+++ LauncherDLL/CoffeeLicense/include/Common.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+#ifdef COFFEELICENSE_EXPORTS
+#define COFFEELICENSE_EXPORT   __declspec(dllexport)
+#else
+#define COFFEELICENSE_EXPORT   __declspec(dllimport)
+#endif
---
+++ LauncherDLL/CoffeeLicense/src/HARDWAREINFO.cpp
@@ -0,0 +1,23 @@
+﻿#include "HARDWAREINFO.h"
+
+const CHardwareInfo* CHardwareInfo::GetInstance()
+{
+    static CHardwareInfo* inst = NULL;
+
+    if (NULL == inst)
+    {
+        inst = new CHardwareInfo();
+    }
+
+    return inst;
+}
+
+std::wstring CHardwareInfo::GetBiosSerial()
+{
+    return m_hwid.SMBIOS.SerialNumber;
+}
+
+std::wstring CHardwareInfo::GetCpuProcessorId()
+{
+    return m_hwid.CPU.ProcessorId;
+}
