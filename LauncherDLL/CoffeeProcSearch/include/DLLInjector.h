0a1bffe94f1039e2eac83e812025f887a68ea027
KJM <kjm99d@naver.com>
2022-12-07 20:48:58 +0900

    ADD: DLL Injector 모듈 로딩 검사 코드 추가

--- LauncherDLL/CoffeeProcSearch/include/DLLInjector.h
+++ LauncherDLL/CoffeeProcSearch/include/DLLInjector.h
@@ -1,13 +1,19 @@
 ﻿#pragma once
 #include "Common.h"
-
-#define INT_INJECTION_SUCCESS   1
-#define INT_OPENPROCESS_FAIL    -10
-#define INT_LOADLIBRARY_FAIL    -20
-#define INT_GETPROCADDRESS_FAIL -30
-#define INT_VIRTUALALLOC_FAIL   -40
+// ==========================================================
+#define INT_INJECTION_SUCCESS           1
+#define INT_INJECTION_ALREADY_EXIST     2
+#define INT_OPENPROCESS_FAIL            -10
+#define INT_LOADLIBRARY_FAIL            -20
+#define INT_GETPROCADDRESS_FAIL         -30
+#define INT_VIRTUALALLOC_FAIL           -40
 #define INT_WRITEPROCESSMEMORY_FAIL     -50
 #define INT_CREATEREMOTETHREAD_FAIL     -60
+// ==========================================================
+// ==================== 인젝션 관련 콜백 ====================
+//  BOOL 타입을 반환하는 인젝션 콜백
+typedef BOOL(*fp_bInjectCallback)();
+// ==========================================================
 
 /**
  * @brief 프로젝트에 DLL을 인젝션 하는 함수
@@ -15,9 +21,8 @@
  * @param pDllPath - DLL의 경로
  * @return 성공여부 성공 시 INT_INJECTION_SUCCESS(1)
  */
-COFFEEPROCSEARCH_API int Inject(const DWORD dwPid, const char* pDllPath);
+COFFEEPROCSEARCH_API int Inject(const DWORD dwPid, const char* pDllPath, fp_bInjectCallback callback);
 COFFEEPROCSEARCH_API int InjectXP(const DWORD dwPid, const char* pDllPath);
 
 
-
-
+COFFEEPROCSEARCH_API BOOL IsModuleExistInProcessId(DWORD dwPID, char* findModule);
--- LauncherDLL/CoffeeProcSearch/src/DLLInjector.cpp
+++ LauncherDLL/CoffeeProcSearch/src/DLLInjector.cpp
@@ -1,20 +1,83 @@
 ﻿#include "DLLInjector.h"
+#include <tlhelp32.h>
 #include <stdio.h>
 
-int Inject(const DWORD dwPid, const char* pDllPath)
+BOOL IsModuleExistInProcessId(DWORD dwPID, char * findModule)
+{
+    HANDLE hModuleSnap = INVALID_HANDLE_VALUE;
+    MODULEENTRY32 me32;
+
+    //  Take a snapshot of all modules in the specified process. 
+    hModuleSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, dwPID);
+    if (hModuleSnap == INVALID_HANDLE_VALUE)
+    {
+        //printError(TEXT("CreateToolhelp32Snapshot (of modules)"));
+        return(FALSE);
+    }
+
+    //  Set the size of the structure before using it. 
+    me32.dwSize = sizeof(MODULEENTRY32);
+
+    //  Retrieve information about the first module, 
+    //  and exit if unsuccessful 
+    if (!Module32First(hModuleSnap, &me32))
+    {
+       //printError(TEXT("Module32First"));  // Show cause of failure 
+        CloseHandle(hModuleSnap);     // Must clean up the snapshot object! 
+        return(FALSE);
+    }
+
+    //  Now walk the module list of the process, 
+    //  and display information about each module 
+    BOOL isFindModule = FALSE;
+
+    do
+    {
+        wprintf_s(L">> ModuleName [%s] \n", me32.szModule);
+        /*
+        _tprintf(TEXT("\n\n     MODULE NAME:     %s"), me32.szModule);
+        _tprintf(TEXT("\n     executable     = %s"), me32.szExePath);
+        _tprintf(TEXT("\n     process ID     = 0x%08X"), me32.th32ProcessID);
+        _tprintf(TEXT("\n     ref count (g)  =     0x%04X"), me32.GlblcntUsage);
+        _tprintf(TEXT("\n     ref count (p)  =     0x%04X"), me32.ProccntUsage);
+        _tprintf(TEXT("\n     base address   = 0x%08X"), (DWORD)me32.modBaseAddr);
+        _tprintf(TEXT("\n     base size      = %d"), me32.modBaseSize);
+        */
+
+        // 여기에 문자열 비교 할 것.
+
+    } while (Module32Next(hModuleSnap, &me32));
+
+   // _tprintf(TEXT("\n"));
+
+    //  Do not forget to clean up the snapshot object. 
+    CloseHandle(hModuleSnap);
+    
+    return isFindModule;
+}
+
+int Inject(const DWORD dwPid, const char* pDllPath, fp_bInjectCallback callback)
 {
     int ret = INT_INJECTION_SUCCESS;
+
+    // 콜백 함수를 호출하고 콜백함수가 성공 시 리턴한다.
+    if (callback) {
+        BOOL bRetCallback = callback();
+        if (TRUE == bRetCallback) {
+            return FALSE;
+        }
+    }
 
     HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, dwPid);
     if (hProcess)
     {
-
         // DLL 로딩 함수 구하고 
         HMODULE hKernel32 = LoadLibrary(L"Kernel32.dll");
         if (NULL == hKernel32)
         {
             return INT_LOADLIBRARY_FAIL;
         }
+
 
         FARPROC lpfnLoadLibrary = GetProcAddress(hKernel32, "LoadLibraryA");
         if (NULL == lpfnLoadLibrary)
@@ -36,13 +99,13 @@ int Inject(const DWORD dwPid, const char* pDllPath)
             int nLenDLL = strlen(pDllPath);
             DWORD oldProtect = 0;
 
-           if (!WriteProcessMemory(hProcess, dllPathAlloc, pDllPath, nLenDLL, NULL))
+            if (!WriteProcessMemory(hProcess, dllPathAlloc, pDllPath, nLenDLL, NULL))
             {
                 printf("WriteProcessMemory Failure %d \n", GetLastError());
 
                 return INT_WRITEPROCESSMEMORY_FAIL;
             }
-            
+
             HANDLE hThread = CreateRemoteThread(hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)lpfnLoadLibrary, dllPathAlloc, NULL, NULL);
             if (hThread == NULL)
             {
@@ -62,8 +125,6 @@ int Inject(const DWORD dwPid, const char* pDllPath)
 
     return ret;
 }
-
-
 
 int InjectXP(const DWORD dwPid, const char* pDllPath)
 {
